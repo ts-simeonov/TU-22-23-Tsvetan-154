1f30aca06fd19c63b0bb77acd7dd4d8e9b56e109
ts-simeonov <116681205+ts-simeonov@users.noreply.github.com>
2022-11-09 01:30:14 +0200

    Add files via upload

---
+++ binary_1_or_0.py
@@ -0,0 +1,3 @@
+z = int(input('Vuvedete chislo:'))
+y = int(input('Koi znak da se proveri:'))
+print(y, "-iqt znak e", str(bin(z))[1+y])
---
+++ odd_even.py
@@ -0,0 +1,5 @@
+z = int(input("Vuvedete chislo za proverka:"))
+if z % 2 == 0:
+    print("Chisloto e chetno")
+else:
+        print("Chisloto e nechetno")
