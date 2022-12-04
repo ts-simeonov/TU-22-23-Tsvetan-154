c3f9787a9a060c985fa4c7d6ad657227badce0d1
ts-simeonov <116681205+ts-simeonov@users.noreply.github.com>
2022-12-04 16:14:51 +0200

    Rename hello.py to HW-1/hello.py

---
+++ HW-1/hello.py
@@ -0,0 +1,2 @@
+# Predpochitam da ne komentiram po vuprosa
+print("Привет, това е repo-то на Цветан.")
--- hello.py
+++
@@ -1,2 +0,0 @@
-# Predpochitam da ne komentiram po vuprosa
-print("Привет, това е repo-то на Цветан.")
